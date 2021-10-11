ca3d495602870031d5c6e80f4fc2eaffad3376bb
Adarsh Balachandran <Adarsh.Balachandran@hatio.in>
2021-10-11 14:03:13 +0530

    New branch dev

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import Button from '@mui/material/Button';
 function App() {
   return (
     <div>
       Mock Server
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
