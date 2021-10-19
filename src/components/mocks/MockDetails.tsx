d044ddd862296130b65fe61195cfb2fe206573e7
Adarsh Balachandran <Adarsh.Balachandran@hatio.in>
2021-10-19 12:05:32 +0530

    Add basic styles to Mock details page

--- src/components/mocks/MockDetails.tsx
+++ src/components/mocks/MockDetails.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent, } from "react";
 import { useHistory } from "react-router";
 import Mock from "../../types/mock";
 import RequestType from "../../types/request_type";
+import '../mocks/Mock.css';
 
 interface MockDetailsProps {
 
@@ -21,15 +22,18 @@ const MockDetails: FunctionComponent<MockDetailsProps> = (props) => {
 
 
 
-    return (<><h2>Details</h2>
-        {mock.name}
-        <br />
+    return (<>
+        <section className="mock-details">
+            <h2>{mock.name}</h2>
+
+            <h5>Params</h5>
+            {<div><pre>{JSON.stringify(req.params, null, 2)}</pre></div>}
+
         <h5>Response</h5>
-        {req === undefined ? "" : Object.entries(req.response)}
-        <h5>Params</h5>
-        {Object.entries(req.params)}
-        <h5>Status</h5>
-        {req.status}
+            {<div><pre>{JSON.stringify(req.response, null, 2)}</pre></div>}
+
+
+        </section>
     </>);
 }
 
