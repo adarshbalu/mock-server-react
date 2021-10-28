c8870a04a4b9103f12b9eae7ad4152e5c03e1584
Adarsh Balachandran <Adarsh.Balachandran@hatio.in>
2021-10-28 12:36:55 +0530

    Change API url for mock-servers

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,7 +1,7 @@
 export default class URL {
     static BASE_URL: string = "http://localhost:6001";
 
-    static API_BASE_URL: string = "http://localhost:3101";
+    static API_BASE_URL: string = "http://localhost:6001/api";
 
     static MOCK_ENDPOINT: string = "/mocks";
     static MOCK_PATH: string = URL.BASE_URL + URL.MOCK_ENDPOINT;
